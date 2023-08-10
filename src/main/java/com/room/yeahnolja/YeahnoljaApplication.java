e835e0159166202d4b4aff811847fb1095a2cc50
Song0-0 <ks08094@naver.com>
2023-08-10 23:01:13 +0900

    git init

---
+++ src/main/java/com/room/yeahnolja/YeahnoljaApplication.java
@@ -0,0 +1,13 @@
+package com.room.yeahnolja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeahnoljaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeahnoljaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/room/yeahnolja/YeahnoljaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.room.yeahnolja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeahnoljaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
