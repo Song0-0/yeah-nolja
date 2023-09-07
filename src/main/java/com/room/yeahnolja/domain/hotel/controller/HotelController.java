75e8fb6c1349aafdd4c87c1e3a46378f0298a3cb
Song0-0 <ks08094@naver.com>
2023-09-07 16:29:34 +0900

    공통된 url은 상단에 묶기

--- src/main/java/com/room/yeahnolja/domain/hotel/controller/HotelController.java
+++ src/main/java/com/room/yeahnolja/domain/hotel/controller/HotelController.java
@@ -2,12 +2,10 @@ package com.room.yeahnolja.domain.hotel.controller;
 
 import com.room.yeahnolja.domain.hotel.dto.HotelRequestDto;
 import com.room.yeahnolja.domain.hotel.dto.HotelResponseDto;
-import com.room.yeahnolja.domain.hotel.entity.Hotel;
 import com.room.yeahnolja.domain.hotel.service.HotelService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,12 +15,13 @@ import java.util.List;
 @Tag(name = "Hotel Controller")
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/hotels")
 public class HotelController {
 
     private final HotelService hotelService;
 
     @Operation(summary = "호텔 단건 등록 (jpa 사용)")
-    @PostMapping("/hotel/save")
+    @PostMapping("/save")
     public ResponseEntity<Void> saveHotel(@RequestBody HotelRequestDto requestDto) {
         try {
             hotelService.saveHotel(requestDto);
@@ -35,7 +34,7 @@ public class HotelController {
     }
 
     @Operation(summary = "호텔 단건 수정")
-    @PatchMapping("/hotel/{hotelId}")
+    @PatchMapping("/{hotelId}")
     public ResponseEntity<Void> modifyHotel(@PathVariable int hotelId, @RequestBody HotelRequestDto requestDto) {
         hotelService.modifyHotel(hotelId, requestDto);
         return ResponseEntity.ok()
@@ -43,7 +42,7 @@ public class HotelController {
     }
 
     @Operation(summary = "호텔 단건 삭제")
-    @DeleteMapping("/hotel/{hotelId}")
+    @DeleteMapping("/{hotelId}")
     public ResponseEntity<Void> removeHotel(@PathVariable int hotelId) {
         hotelService.removeHotel(hotelId);
         return ResponseEntity.ok()
@@ -52,7 +51,7 @@ public class HotelController {
 
 
     @Operation(summary = "호텔 전체 조회")
-    @GetMapping("/hotels")
+    @GetMapping("")
     public ResponseEntity<List<HotelResponseDto>> getAllHotels() {
         List<HotelResponseDto> allHotels = hotelService.getAllHotels();
         return ResponseEntity.ok()
@@ -60,7 +59,7 @@ public class HotelController {
     }
 
     @Operation(summary = "호텔 단건 조회")
-    @GetMapping("/hotels/{hotelId}")
+    @GetMapping("/{hotelId}")
     public ResponseEntity<HotelResponseDto> getHotel(@PathVariable int hotelId) {
         HotelResponseDto hotel = hotelService.getHotel(hotelId);
         return ResponseEntity.ok()
@@ -68,7 +67,7 @@ public class HotelController {
     }
 
     @Operation(summary = "특정 지역에 대한 호텔 조회")
-    @GetMapping("/hotels/location")
+    @GetMapping("/location")
     public ResponseEntity<List<HotelResponseDto>> getHotelsByLocation(@RequestParam String location) {
         List<HotelResponseDto> hotelsByLocation = hotelService.getHotelsByLocation(location);
         return ResponseEntity.ok()
@@ -76,7 +75,7 @@ public class HotelController {
     }
 
     @Operation(summary = "특정 가격에 대한 호텔 조회")
-    @GetMapping("/hotels/price")
+    @GetMapping("/price")
     public ResponseEntity<List<HotelResponseDto>> getHotelsByPrice(@RequestParam int price) {
         List<HotelResponseDto> hotelsByPrice = hotelService.getHotelsByPrice(price);
         return ResponseEntity.ok()
@@ -84,7 +83,7 @@ public class HotelController {
     }
 
     @Operation(summary = "특정 호텔명에 대한 호텔 조회")
-    @GetMapping("/hotels/name")
+    @GetMapping("/name")
     public ResponseEntity<List<HotelResponseDto>> getHotelsByName(@RequestParam String name) {
         List<HotelResponseDto> hotelsByName = hotelService.getHotelsByName(name);
         return ResponseEntity.ok()
